ffb4a42ed63e7166bb365d3ce0090d83a3e2181d
Tram Bui <pbui17@cmc.edu>
2020-12-18 10:59:01 -0600

    got the second wave to pass

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ import TimelineEvent from './components/TimelineEvent';
 function App() {
   console.log(timelineData);
 
+  const events = [
+    {
+      person: 'Grace Hopper',
+      status: 'Ada is a strongly typed language. C++ is a strongly hyped language.',
+      timeStamp: '2018-05-18T22:12:03Z'
+    },
+    {
+      person: 'Richard O\'Keefe',
+      status: 'Fifty years of programming language research, and we end up with C++ ???',
+      timeStamp: '2018-05-18T22:19:40Z'
+    }
+  ];
+
   // Customize the code below
   return (
     <div className="App">
@@ -17,6 +30,7 @@ function App() {
       </header>
       <main className="App-main">
         <TimelineEvent person="Ada Lovelace" status="The Original programmer... old-school style, on PAPER." timeStamp="2018-05-18T22:12:03Z" />
+        <Timeline events={events}/>
       </main>
     </div>
   );
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  const timelineEventComponents = props.events.map ((item,i) => {
+    return (
+      <TimelineEvent
+        person={item.person}
+        status={item.status}
+        timeStamp={item.timeStamp}
+        key={i}
+      />
+    )
+  })
+
+  return(
+    <div>
+      {timelineEventComponents}
+    </div>
+    
+  )
 }
 
 export default Timeline;
