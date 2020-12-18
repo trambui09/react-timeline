830df5f748b1c893cb1da8ecb6c2d2732d577a37
Tram Bui <pbui17@cmc.edu>
2020-12-18 10:30:41 -0600

    installed the initial dependencies and started working on the TimelineEvent component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
+
 
 function App() {
   console.log(timelineData);
@@ -11,9 +13,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">Ada Lovelace's social media feed</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent person="Tram" status="Available" timestamp="12:33:44" />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return(
+    <div>
+      <p>{props.person}</p>
+      <p>{props.status}</p>
+      <Timestamp timestamp="2018-05-18T22:12:03Z" />
+    </div>
+  )
 }
 
 export default TimelineEvent;
